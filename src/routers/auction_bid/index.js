485eb119740e6d64b345b82e3a8ab9782e03b17f
datnt <datnt06039@gmail.com>
2020-08-02 01:28:42 +0700

    2/8/2020

--- src/routers/auction_bid/index.js
+++ src/routers/auction_bid/index.js
@@ -4,23 +4,6 @@ const bodyParser = require('body-parser')
 const createAuctionBidModel = ({ AuctionBid }) => {
     const router = express.Router()
 
-        // get all person bid that product at that aution
-        router.get('/', async (req, res) => {
-            // find by primary key = find by id
-            const products = await Product.findAll({
-                include: [{
-                    model: User,
-                    where: {id: req.params.user_id},
-                    required: false,
-                   }]
-                }).then(products => {
-                    if(products){
-                        res.send(products)
-                    } else {
-                        res.sendStatus(404)
-                    }
-                });
-        })
 
     return router
 }
